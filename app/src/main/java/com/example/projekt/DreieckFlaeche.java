84eb6d1cc996cf1f460a30c5f95fa2f919ec50ea
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-05 12:39:19 +0100

    DreieckFläche und DreieckUmfang bearbeitet, damit das berechnen klappt

--- app/src/main/java/com/example/projekt/DreieckFlaeche.java
+++ app/src/main/java/com/example/projekt/DreieckFlaeche.java
@@ -3,25 +3,57 @@ package com.example.projekt;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DreieckFlaeche extends AppCompatActivity {
 
-    EditText etUa;
 
     Button btZurueckDFI;
+    EditText etDFa;
+    EditText etDFh;
+    Button btDFB;
+    TextView tvDFE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dreieck_flaeche);
 
-        etUa = findViewById(R.id.etUa);
         btZurueckDFI =findViewById(R.id.btZurueckDFI);
 
         btZurueckDFI.setOnClickListener(View -> clickedZurueckDU());
 
+        btDFB = findViewById(R.id.btDFB);
+
+        btDFB.setOnClickListener(View -> clickedDFB());
+
+        etDFa = findViewById(R.id.etDFa);
+        etDFh = findViewById(R.id.etDFh);
+        tvDFE = findViewById(R.id.tvDFE);
+
+    }
+
+    private void clickedDFB() {
+        String dfastr = etDFa.getText().toString();
+        String dfhstr = etDFh.getText().toString();
+        //double dfa = Double.parseDouble(etDFa.getText().toString());
+        //double dfh = Double.parseDouble(etDFh.getText().toString());
+
+
+
+
+        if (dfastr.length()==0||dfhstr.length()==0){
+            tvDFE.setText("Bitte alle geforderten Werte eintragen!");
+
+        }
+        else {
+            double dfa = Double.parseDouble(dfastr);
+            double dfh = Double.parseDouble(dfhstr);
+            double dfe = 0.5 * dfa * dfh;
+            tvDFE.setText("Flächeninhalt: " + dfe);
+        }
     }
 
     private void clickedZurueckDU() {
--- app/src/main/java/com/example/projekt/DreieckUmfang.java
+++ app/src/main/java/com/example/projekt/DreieckUmfang.java
@@ -2,12 +2,19 @@ package com.example.projekt;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DreieckUmfang extends AppCompatActivity {
 
     Button btZurueckDU;
+    EditText etDUa;
+    EditText etDUb;
+    EditText etDUc;
+    Button btDUB;
+    TextView tvDUE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,27 @@ public class DreieckUmfang extends AppCompatActivity {
         btZurueckDU = findViewById(R.id.btZurueckDU);
 
         btZurueckDU.setOnClickListener(View -> clickedZDU());
+
+        btDUB = findViewById(R.id.btDUB);
+
+        btDUB.setOnClickListener(View -> clickedDUB());
+
+        etDUa = findViewById(R.id.etDUa);
+        etDUb = findViewById(R.id.etDUb);
+        etDUc = findViewById(R.id.etDUc);
+        tvDUE = findViewById(R.id.tvDUE);
+
+    }
+
+    private void clickedDUB() {
+        double dua = Double.parseDouble(etDUa.getText().toString());
+        double dub = Double.parseDouble(etDUb.getText().toString());
+        double duc = Double.parseDouble(etDUc.getText().toString());
+        double due = dua+dub+duc;
+        tvDUE.setText("Umfang: " + due);
+
+
+
 
     }
 
