da3046ab5d2779fd95ede093304f45ecd4b17c12
Sebastian <s.hesse@hgs-singen.online>
2022-01-06 07:00:10 +0100

    Trapez Fläche berechnen

--- app/src/main/java/com/example/projekt/TrapezFlaeche.java
+++ app/src/main/java/com/example/projekt/TrapezFlaeche.java
@@ -2,12 +2,19 @@ package com.example.projekt;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TrapezFlaeche extends AppCompatActivity {
 
     Button btZurueckTFI;
+    EditText etTFIa;
+    EditText etTFIc;
+    EditText etTFIh;
+    Button btTFIB;
+    TextView tvTFIE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +22,34 @@ public class TrapezFlaeche extends AppCompatActivity {
         setContentView(R.layout.activity_trapez_flaeche);
 
         btZurueckTFI = findViewById(R.id.btZurueckTFI);
+        btTFIB = findViewById(R.id.btTFIB);
+        etTFIa = findViewById(R.id.etTFIa);
+        etTFIc = findViewById(R.id.etTFIc);
+        etTFIh = findViewById(R.id.etTFIh);
+        tvTFIE = findViewById(R.id.tvTFIE);
 
         btZurueckTFI.setOnClickListener(View -> clickedZTFI());
+        btTFIB.setOnClickListener(View -> clickedTFIB());
 
+
+    }
+
+    private void clickedTFIB() {
+        String tfiastr = etTFIa.getText().toString();
+        String tficstr = etTFIc.getText().toString();
+        String tfihstr = etTFIh.getText().toString();
+
+        if (tfiastr.length()==0||tficstr.length()==0||tfihstr.length()==0){
+            tvTFIE.setText("Bitte alle geforderten Werte eintragen!");
+
+        }
+        else{
+            double tfia = Double.parseDouble(tfiastr);
+            double tfic = Double.parseDouble(tficstr);
+            double tfih = Double.parseDouble(tfihstr);
+            double tfie = (tfia + tfic) * 0.5 * tfih;
+            tvTFIE.setText("Flächeninhalt: " + tfie + "cm");
+        }
     }
 
     private void clickedZTFI() {
