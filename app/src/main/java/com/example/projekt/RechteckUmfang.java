4bd568af9a447acdf98b58fcbad3676a2e3887d7
Sebastian <s.hesse@hgs-singen.online>
2022-01-06 14:30:51 +0100

    Rechteck Umfang berechnen

--- app/src/main/java/com/example/projekt/RechteckUmfang.java
+++ app/src/main/java/com/example/projekt/RechteckUmfang.java
@@ -2,12 +2,18 @@ package com.example.projekt;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RechteckUmfang extends AppCompatActivity {
 
     Button btZurueckRU;
+    Button btRUB;
+    EditText etRUa;
+    EditText etRUb;
+    TextView tvRUe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,28 @@ public class RechteckUmfang extends AppCompatActivity {
         setContentView(R.layout.activity_rechteck_umfang);
 
         btZurueckRU = findViewById(R.id.btZurueckRU);
+        btRUB = findViewById(R.id.btRUB);
+        etRUa = findViewById(R.id.etRUa);
+        etRUb = findViewById(R.id.etRUb);
+        tvRUe = findViewById(R.id.tvRUe);
 
         btZurueckRU.setOnClickListener(View -> clickedZRU());
+        btRUB.setOnClickListener(View -> clickedRUB());
+    }
+
+    private void clickedRUB() {
+        String ruastr = etRUa.getText().toString();
+        String rubstr = etRUb.getText().toString();
+
+        if (ruastr.length()==0||rubstr.length()==0){
+            tvRUe.setText("Bitte alle geforderten Werte eintragen!");
+        }
+        else {
+            double rua = Double.parseDouble(ruastr);
+            double rub = Double.parseDouble(rubstr);
+            double rue = 2 * (rua + rub);
+            tvRUe.setText("Umfang: " + rue + "cm");
+        }
     }
 
     private void clickedZRU() {
