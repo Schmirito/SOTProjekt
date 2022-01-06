bd62ee45136beda3cf06e25e2c467f76e5cdf16d
Sebastian <s.hesse@hgs-singen.online>
2022-01-06 15:20:51 +0100

    Rechteck Fläche  berechnen

--- app/src/main/java/com/example/projekt/RechteckFlaeche.java
+++ app/src/main/java/com/example/projekt/RechteckFlaeche.java
@@ -2,12 +2,18 @@ package com.example.projekt;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RechteckFlaeche extends AppCompatActivity {
 
     Button btZurueckRFI;
+    Button btRFB;
+    EditText etRFa;
+    EditText etRFb;
+    TextView tvRFe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,29 @@ public class RechteckFlaeche extends AppCompatActivity {
         setContentView(R.layout.activity_rechteck_flaeche);
 
         btZurueckRFI = findViewById(R.id.btZurueckRFI);
+        btRFB = findViewById(R.id.btRFB);
+        etRFa = findViewById(R.id.etRFa);
+        etRFb = findViewById(R.id.etRFb);
+        tvRFe = findViewById(R.id.tvRFe);
 
         btZurueckRFI.setOnClickListener(View -> clickedZRFI());
+        btRFB.setOnClickListener(View -> clickedRFB());
+    }
 
+    private void clickedRFB() {
+        String rfastr = etRFa.getText().toString();
+        String rfbstr = etRFb.getText().toString();
+
+        if (rfastr.length()==0||rfbstr.length()==0){
+            tvRFe.setText("Bitte alle geforderten Werte eingeben!");
+        }
+        else{
+            double rfa = Double.parseDouble(rfastr);
+            double rfb = Double.parseDouble(rfbstr);
+            double rfe = rfa * rfb;
+
+            tvRFe.setText("Flächeninhalt: " + rfe + "cm");
+        }
     }
 
     private void clickedZRFI() {
