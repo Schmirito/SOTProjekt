b9fc19c05372330e9519d84879b1d9fb34bbc688
Sebastian <s.hesse@hgs-singen.online>
2022-01-07 17:03:30 +0100

    prisma update

--- app/src/main/java/com/example/projekt/PrismenDreieckVolumen.java
+++ app/src/main/java/com/example/projekt/PrismenDreieckVolumen.java
@@ -1,13 +1,26 @@
 package com.example.projekt;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PrismenDreieckVolumen extends AppCompatActivity {
 
+    Button btZurueckPriDV;
+    Button btPriDVB;
+    EditText etPriDVa;
+    EditText etPriDVha;
+    EditText etPriDVh;
+    TextView tvPriDVe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prismen_dreieck_volumen);
+
+
+
     }
 }
