562c05cf33628d910de77bff1efde21002135589
Sebastian <s.hesse@hgs-singen.online>
2022-01-07 15:24:41 +0100

    Pyramide mit Viereckiger grundfläche Volumen berechnen (probleme mit berechnung)

--- app/src/main/java/com/example/projekt/PyramideViereckAuswahl.java
+++ app/src/main/java/com/example/projekt/PyramideViereckAuswahl.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 public class PyramideViereckAuswahl extends AppCompatActivity {
 
     Button btZurueckPVA;
+    Button btPVVolumen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,15 @@ public class PyramideViereckAuswahl extends AppCompatActivity {
         setContentView(R.layout.activity_pyramide_viereck_auswahl);
 
         btZurueckPVA = findViewById(R.id.btZurueckPVA);
+        btPVVolumen = findViewById(R.id.btPVVolumen);
 
         btZurueckPVA.setOnClickListener(View -> clickedZPVA());
+        btPVVolumen.setOnClickListener(View -> clickedPVV());
+    }
 
+    private void clickedPVV() {
+        Intent iPVV = new Intent(this, PyramideViereckVolumen.class);
+        startActivity(iPVV);
     }
 
     private void clickedZPVA() {
---
+++ app/src/main/java/com/example/projekt/PyramideViereckVolumen.java
@@ -0,0 +1,53 @@
+package com.example.projekt;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PyramideViereckVolumen extends AppCompatActivity {
+
+    Button btZurueckPVV;
+    Button btPVVB;
+    EditText etPVVa;
+    EditText etPVVh;
+    TextView tvPVVe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pyramide_viereck_volumen);
+
+        btZurueckPVV = findViewById(R.id.btZurueckPVV);
+        btPVVB = findViewById(R.id.btPVVB);
+        etPVVa = findViewById(R.id.etPVVa);
+        etPVVh = findViewById(R.id.etPVVh);
+        tvPVVe = findViewById(R.id.tvPVVe);
+
+        btZurueckPVV.setOnClickListener(View -> clickedZPVV());
+        btPVVB.setOnClickListener(View -> clickedPVVB());
+    }
+
+    private void clickedPVVB() {
+        String pvvastr = etPVVa.getText().toString();
+        String pvvhstr = etPVVh.getText().toString();
+
+        if (pvvastr.length()==0||pvvhstr.length()==0){
+            tvPVVe.setText("Bitte alle geforderten Werte eingeben");
+        }
+        else{
+            double pvva = Double.parseDouble(pvvastr);
+            double pvvh = Double.parseDouble(pvvhstr);
+            double pvve = 0.33 * (pvva * pvva) * pvvh;
+
+            tvPVVe.setText("Volumen: " + pvve + "cm³");
+        }
+    }
+
+    private void clickedZPVV() {
+        Intent iZPVV = new Intent(this,PyramideViereckAuswahl.class);
+        startActivity(iZPVV);
+    }
+}
