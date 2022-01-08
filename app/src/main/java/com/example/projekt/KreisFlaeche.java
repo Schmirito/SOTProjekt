1f9c012ce379024270429b86ecd6f3e205ac1318
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-08 22:01:20 +0100

    Kreis Fläche

--- app/src/main/java/com/example/projekt/KreisFlaeche.java
+++ app/src/main/java/com/example/projekt/KreisFlaeche.java
@@ -1,13 +1,47 @@
 package com.example.projekt;
 
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class KreisFlaeche extends AppCompatActivity {
-
+    Button btZurueckKF;
+    EditText etKFD;
+    Button btKFB;
+    TextView tvKFE;
+    double pi = Math.PI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kreis_flaeche);
+
+        btZurueckKF = findViewById(R.id.btZurueckKF);
+        btKFB = findViewById(R.id.btKFB);
+        etKFD = findViewById(R.id.etKFD);
+        tvKFE = findViewById(R.id.tvKFE);
+
+        btZurueckKF.setOnClickListener(view -> clickedZKF());
+        btKFB.setOnClickListener(view -> clickedKFB());
+    }
+
+    private void clickedKFB() {
+        String kfdstr = etKFD.getText().toString();
+        if (kfdstr.length()==0){
+            tvKFE.setText("Bitte alle geforderten Werte eintragen!");
+
+        }
+        else {
+            double kfd = Double.parseDouble(kfdstr);
+            double kfe = pi/4*kfd*kfd;
+            tvKFE.setText("Umfang: " + kfe + " cm");
+        }
+    }
+
+    private void clickedZKF() {
+        Intent iZKF = new Intent(this, KugelAuswahl.class);
+        startActivity(iZKF);
     }
 }
