486b4547653d709350c4c6e9d3b3fc2b78c4b1cc
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-08 18:15:16 +0100

    activities added

---
+++ app/src/main/java/com/example/projekt/KreisFlaeche.java
@@ -0,0 +1,13 @@
+package com.example.projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KreisFlaeche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kreis_flaeche);
+    }
+}
---
+++ app/src/main/java/com/example/projekt/KugelVolumen.java
@@ -0,0 +1,13 @@
+package com.example.projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KugelVolumen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kugel_volumen);
+    }
+}
