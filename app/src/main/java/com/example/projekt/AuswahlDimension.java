105a9fa6ba846d7af0b88d5fb9d9752c3578abab
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-10 20:32:38 +0100

    Einleitungsseite hinzugefügt

--- app/src/main/java/com/example/projekt/AuswahlDimension.java
+++ app/src/main/java/com/example/projekt/AuswahlDimension.java
@@ -5,29 +5,25 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ErsteActivity extends AppCompatActivity {
-
-
-
-   /* Button bt2D;
-    Button bt3D;*/
+public class AuswahlDimension extends AppCompatActivity {
+    Button bt2D;
+    Button bt3D;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_erste);
-
-        /*bt2D = findViewById(R.id.bt2D);
+        setContentView(R.layout.activity_auswahl_dimension);
+        bt2D = findViewById(R.id.bt2D);
         bt3D = findViewById(R.id.bt3D);
 
 
         bt2D.setOnClickListener(View -> clicked2D());
 
-        bt3D.setOnClickListener(View -> clicked3D());*/
+        bt3D.setOnClickListener(View -> clicked3D());
 
     }
 
-    /*private void clicked3D() {
+    private void clicked3D() {
         Intent i3D = new Intent(this, DreiDAuswahl.class);
         startActivity(i3D);
     }
@@ -35,6 +31,5 @@ public class ErsteActivity extends AppCompatActivity {
     private void clicked2D() {
         Intent i2D = new Intent(this, ZweiDAuswahl.class);
         startActivity(i2D);
-    }*/
+    }
 }
-
