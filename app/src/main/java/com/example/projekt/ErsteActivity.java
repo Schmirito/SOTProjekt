17f50ff1e243db7c1682999bb686a376e8f2d322
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-10 20:36:26 +0100

    Button verknüpft

--- app/src/main/java/com/example/projekt/ErsteActivity.java
+++ app/src/main/java/com/example/projekt/ErsteActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ErsteActivity extends AppCompatActivity {
+    Button btAuswahlDimension;
 
 
 
@@ -17,24 +18,15 @@ public class ErsteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erste);
 
-        /*bt2D = findViewById(R.id.bt2D);
-        bt3D = findViewById(R.id.bt3D);
+        btAuswahlDimension = findViewById(R.id.btAuswahlDimension);
 
-
-        bt2D.setOnClickListener(View -> clicked2D());
-
-        bt3D.setOnClickListener(View -> clicked3D());*/
+        btAuswahlDimension.setOnClickListener(View -> clickedAuswahlDimension());
 
     }
 
-    /*private void clicked3D() {
-        Intent i3D = new Intent(this, DreiDAuswahl.class);
-        startActivity(i3D);
+    private void clickedAuswahlDimension() {
+        Intent iAD = new Intent(this, AuswahlDimension.class);
+        startActivity(iAD);
     }
-
-    private void clicked2D() {
-        Intent i2D = new Intent(this, ZweiDAuswahl.class);
-        startActivity(i2D);
-    }*/
 }
 
