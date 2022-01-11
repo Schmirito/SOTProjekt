3bceb2227a6abf71c01766b0942c960ef048f19c
Feuerwehrann <a.manegold@hgs-singen.online>
2022-01-11 18:11:03 +0100

    Activity Umrechner eingefügt

---
+++ app/src/main/java/com/example/projekt/Umrechner.java
@@ -0,0 +1,13 @@
+package com.example.projekt;
+
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Umrechner extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umrechner);
+    }
+}
